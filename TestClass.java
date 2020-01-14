ac489597a6be2c5ef4875eb2c1b8de1b908a6ca1
nayoung <rnjsskdud941@naver.com>
2020-01-14 15:04:57 +0900

    test commit

--- TestClass.java
+++ TestClass.java
@@ -1,6 +1,6 @@
 package test;
 public class TestClass {
-    public void static main (String[] args) {
-        System.out.println("Hello!");
+    public static void  main (String[] args) {
+        System.out.println("Hello!!~~");
     }
 }

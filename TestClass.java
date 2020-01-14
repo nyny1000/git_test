95e140b7ef66eabc52e1d5255f8356d7cbeaccda
nyny1000 <59862783+nyny1000@users.noreply.github.com>
2020-01-14 16:08:29 +0900

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -2,6 +2,6 @@ package test;
 public class TestClass {
     public static void  main (String[] args) {
         System.out.println("new branch!!~~");
-        System.out.println("hungry!!~~");
+        System.out.println("very hungry!!~~");
     }
 }

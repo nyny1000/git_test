f92f255a2e3825ef55896302d772cc19df1aba54
nayoung <rnjsskdud941@naver.com>
2020-01-14 15:49:15 +0900

    add gitignore

--- TestClass.java
+++ TestClass.java
@@ -1,7 +1,7 @@
 package test;
 public class TestClass {
     public static void  main (String[] args) {
-        System.out.println("ohio!!~~");
+        System.out.println("new branch!!~~");
         System.out.println("hungry!!~~");
     }
 }
